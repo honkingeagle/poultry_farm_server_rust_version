eb91e712184fa255f7e5f1f1efa22f19a6702dfb
Tiras Mwangi <tmwangi599@gmail.com>
2023-06-13 21:27:23 +0300

    made new sql file

--- migrations/20230606201613_poultry_farm.sql
+++ migrations/20230606201613_poultry_farm.sql
@@ -1,4 +1,5 @@
 -- Add migration script here
+-- Add migration script here
 DROP TABLE IF EXISTS sessions;
 
 CREATE TABLE IF NOT EXISTS users (
@@ -10,10 +11,16 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS farms (
-    id serial primary key references users(id),
+    id serial primary key,
+    user_id int not null,
     name varchar(255) not null,
     createdAt timestamp default current_timestamp,
     updatedAt timestamp default current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS user_farms (
+    user_id int references users(id),
+    farm_id int references farms(id)
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
