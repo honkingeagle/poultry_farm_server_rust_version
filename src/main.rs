31291f634b092735e863b71f2a4d7d40f339cfcb
Tiras Mwangi <tmwangi599@gmail.com>
2023-06-18 14:45:28 +0300

    Improved error handling with unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use poultry_farm_server::AppState;
 use shuttle_secrets::SecretStore;
 use sqlx::postgres::PgPoolOptions;
+use std::process;
 
 #[shuttle_runtime::main]
 async fn axum(#[shuttle_secrets::Secrets] secrets: SecretStore) -> shuttle_axum::ShuttleAxum {
@@ -8,12 +9,18 @@ async fn axum(#[shuttle_secrets::Secrets] secrets: SecretStore) -> shuttle_axum:
         .max_connections(5)
         .connect(&secrets.get("DATABASE_URL").unwrap())
         .await
-        .expect("Unable to load database_url");
+        .unwrap_or_else(|err| {
+            println!("Unable to load database_url: {err}");
+            process::exit(1);
+        });
 
     sqlx::migrate!()
         .run(&pool)
         .await
-        .expect("Unable to migrate sql files");
+        .unwrap_or_else(|err| {
+            println!("Unable to migrate sql files: {err}");
+            process::exit(1);
+        });
 
     let state = AppState::new(pool);
 
