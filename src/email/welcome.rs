a8aa5fbc8dfb5e1e1a615380566fd6a09f1aeda3
Tiras Mwangi <tmwangi599@gmail.com>
2023-08-08 21:04:46 +0300

    added welcome email functionality which has an email activation link

--- src/email/welcome.rs
+++ src/email/welcome.rs
@@ -4,25 +4,45 @@ use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 
 pub async fn mail(state: AppState, mail: &str) {
-    let email = Message::builder()
-        .from("noreply <nobody@gardenofeden.rs>".parse().unwrap())
-        .to(mail.parse().unwrap())
-        .subject("Welcome to The Garden Of Eden")
-        .header(ContentType::TEXT_PLAIN)
-        .body(String::from(
-            "Enter the Garden Of Eden. I'll let you partake the fruit🍑",
-        ))
-        .unwrap();
+    let activation_token: u128 = rand::random();
 
-    let creds = Credentials::new(state.smtp_email, state.smtp_password);
+    let activation_token = activation_token.to_string();
 
-    let mailer = SmtpTransport::relay("smtp.gmail.com")
-        .unwrap()
-        .credentials(creds)
-        .build();
+    let query = sqlx::query("INSERT INTO activations (email, activation_id) VALUES ($1, $2)")
+        .bind(mail)
+        .bind(&activation_token)
+        .execute(&state.pool)
+        .await;
 
-    if let Err(err) = mailer.send(&email) {
-        eprintln!("{:?}", err);
+    match query {
+        Ok(_) => {
+            let email = Message::builder()
+                .from("noreply <nobody@gardenofeden.rs>".parse().unwrap())
+                .to(mail.parse().unwrap())
+                .subject("Email verification")
+                .header(ContentType::TEXT_HTML)
+                .body(format!(
+                    "<div>
+                    <h1>Enter the Garden Of Eden.</h1>
+                    I'll let you partake the fruit. Only if you 
+                    <a href='http://127.0.0.1:8000/users/activate/{mail}/{activation_token}'>
+                        Activate your account
+                    </a>
+                </div>"
+                ))
+                .unwrap();
+            let creds = Credentials::new(state.smtp_email, state.smtp_password);
+
+            let mailer = SmtpTransport::relay("smtp.gmail.com")
+                .unwrap()
+                .credentials(creds)
+                .build();
+
+            if let Err(err) = mailer.send(&email) {
+                eprintln!("{:?}", err);
+            }
+        }
+        Err(err) => eprintln!("Something happened: {}", err),
     }
 }
 
@@ -38,15 +58,18 @@ mod tests {
             .from("GardenOfEden <nobody@gardenofeden.rs>".parse().unwrap())
             .to("<tmwangi599@gmail.com>".parse().unwrap())
             .subject("Welcome to GardenOfEden")
-            .header(ContentType::TEXT_PLAIN)
+            .header(ContentType::TEXT_HTML)
             .body(String::from(
-                "Enter the Garden Of Eden. I'll let you partake the fruit",
+                "<div>
+                    <h1>Enter the Garden Of Eden.</h1>
+                    I'll let you partake the fruit. Only if you verify
+                </div>",
             ))
             .unwrap();
 
         // To get your smtp relay to work with gmail
         // enable 2-FA then autogenerate passwords for a specific app
-        let creds = Credentials::new("your_email_address".to_owned(), "your_password".to_owned());
+        let creds = Credentials::new("your_email".to_owned(), "your_password".to_owned());
 
         let mailer = SmtpTransport::relay("smtp.gmail.com")
             .unwrap()
